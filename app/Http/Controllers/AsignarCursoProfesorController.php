d568647f284b43aa8f52024880f6dec061204452
Alexis-chata <alexizz.19.ac@gmail.com>
2021-01-18 15:27:09 -0500

    Model, Factory, Migrate, Seed, Controller de todas las tablas

---
+++ app/Http/Controllers/AsignarCursoProfesorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Asignar_curso_profesor;
+use Illuminate\Http\Request;
+
+class AsignarCursoProfesorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Asignar_curso_profesor  $asignar_curso_profesor
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Asignar_curso_profesor $asignar_curso_profesor)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Asignar_curso_profesor  $asignar_curso_profesor
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Asignar_curso_profesor $asignar_curso_profesor)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Asignar_curso_profesor  $asignar_curso_profesor
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Asignar_curso_profesor $asignar_curso_profesor)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Asignar_curso_profesor  $asignar_curso_profesor
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Asignar_curso_profesor $asignar_curso_profesor)
+    {
+        //
+    }
+}
---
+++ database/factories/AsignarCursoProfesorFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Asignar_curso_profesor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AsignarCursoProfesorFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Asignar_curso_profesor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
