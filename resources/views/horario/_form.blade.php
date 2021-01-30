ac0f85fa74e7603f9d24d988acf74d8319fbf4ae
Alexis-chata <alexizz.19.ac@gmail.com>
2021-01-30 17:02:58 -0500

    avance2

--- resources/views/horario/_form.blade.php
+++ resources/views/horario/_form.blade.php
@@ -22,19 +22,25 @@
             <label for="{{ $value->name }}" class="custom-control-label">{{ Str::substr($value->name, 0, 2) }}</label>
         </div>
     @endforeach
-    <div class="">
+    <div class="mx-2">
         <select name="hora_inicio" class="form-control">
             <option value="">-- hora_inicio --</option>
             @php
                 intval(env('HORA_ACADEMICA'));
-                echo date('g:ia');
+                $mifecha= date('H:i',0);
+                $NuevaFecha = strtotime ( '-11 hour' , strtotime ($mifecha) ) ;
+                for ($i=0; $i < 6; $i++) {
+                    # code...
+                }
+                $NuevaFecha = strtotime ( env('HORA_ACADEMICA') , $NuevaFecha ) ;
+                $NuevaFecha = date ( 'H:i' , $NuevaFecha);
             @endphp
-            @foreach ($asignar_curso_profesors as $value)
-            <option value="{{$value->id}}" {{ $horario->asignar_curso_profesors_id==$value->id ? "selected" : ""}}>{{ idate('h') }}</option>
-            @endforeach
+            @for ($i = 0; $i < $env('NRO_HORA_ACADEMICA'); $i++)
+            <option value="{{$NuevaFecha}}">{{ $NuevaFecha }}</option>
+            @endfor
         </select>
     </div>
-    <div class="">
+    <div class="mx-2">
         <select name="hora_fin" class="form-control">
             <option value="">-- hora_fin --</option>
             @foreach ($asignar_curso_profesors as $value)
@@ -43,8 +49,11 @@
         </select>
     </div>
     <div class="">
-        <button type="submit" class="btn btn-primary">{{ $btn_name }}</button>
+        <button type="submit" class="btn btn-primary ml-2">{{ $btn_name }}</button>
     </div>
+    @for ($i = 1; $i < 256; $i++)
+        ping 172.16.1.{{ $i }}<br>
+    @endfor
     @if (!empty($put))
         <input type="hidden" name="_method" value="PUT">
     @endif
