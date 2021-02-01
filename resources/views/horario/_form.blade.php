8a299fc23c32db4405bd53ce0f02da5493fe4482
Alexis-chata <alexizz.19.ac@gmail.com>
2021-02-01 17:01:06 -0500

    avance4

--- resources/views/horario/_form.blade.php
+++ resources/views/horario/_form.blade.php
@@ -26,34 +26,48 @@
         <select name="hora_inicio" class="form-control">
             <option value="">-- hora_inicio --</option>
             @php
-                intval(env('HORA_ACADEMICA'));
-                $mifecha= date('H:i',0);
-                $NuevaFecha = strtotime ( '-11 hour' , strtotime ($mifecha) ) ;
-                for ($i=0; $i < 6; $i++) {
-                    # code...
+            $hr_inicio = strtotime ( env('HORA_INICIO_CLASES') ) ;
+            $hr_inicio = date ( 'H:i' , $hr_inicio);
+            for ($i=0; $i < env('NRO_HORA_ACADEMICA')+1; $i++){
+                if ($hr_inicio!=env('HORA_REFRIGERIO')) {
+                    echo '<option value="'.$hr_inicio.'"> '.$hr_inicio.' </option>';
+                    $hr_inicio = strtotime ( env('HORA_ACADEMICA'), strtotime ($hr_inicio) ) ;
+                    $hr_inicio = date('H:i',$hr_inicio);
+                }else {
+                    //echo '<option value="'.$hr_inicio.'"> '.$hr_inicio.' </option>';
+                    $hr_inicio = strtotime ( env('TIEMPO_REFRIGERIO'), strtotime ($hr_inicio) ) ;
+                    $hr_inicio = date('H:i',$hr_inicio);
                 }
-                $NuevaFecha = strtotime ( env('HORA_ACADEMICA') , $NuevaFecha ) ;
-                $NuevaFecha = date ( 'H:i' , $NuevaFecha);
+
+            }
             @endphp
-            @for ($i = 0; $i < $env('NRO_HORA_ACADEMICA'); $i++)
-            <option value="{{$NuevaFecha}}">{{ $NuevaFecha }}</option>
-            @endfor
         </select>
     </div>
     <div class="mx-2">
         <select name="hora_fin" class="form-control">
             <option value="">-- hora_fin --</option>
-            @foreach ($asignar_curso_profesors as $value)
-            <option value="{{$value->id}}" {{ $horario->asignar_curso_profesors_id==$value->id ? "selected" : ""}}>{{ $value->cursos->name }}</option>
-            @endforeach
+            @php
+            $hr_inicio = strtotime ( env('HORA_INICIO_CLASES') ) ;
+            $hr_inicio = strtotime ( env('HORA_ACADEMICA'), $hr_inicio ) ;
+            $hr_inicio = date ( 'H:i' , $hr_inicio);
+            for ($i=0; $i < env('NRO_HORA_ACADEMICA')+1; $i++){
+                if ($hr_inicio!=env('HORA_REFRIGERIO')) {
+                    echo '<option value="'.$hr_inicio.'"> '.$hr_inicio.' </option>';
+                    $hr_inicio = strtotime ( env('HORA_ACADEMICA'), strtotime ($hr_inicio) ) ;
+                    $hr_inicio = date('H:i',$hr_inicio);
+                }else {
+                    //echo '<option value="'.$hr_inicio.'"> '.$hr_inicio.' </option>';
+                    $hr_inicio = strtotime ( env('TIEMPO_REFRIGERIO'), strtotime ($hr_inicio) ) ;
+                    $hr_inicio = date('H:i',$hr_inicio);
+                }
+
+            }
+            @endphp
         </select>
     </div>
     <div class="">
         <button type="submit" class="btn btn-primary ml-2">{{ $btn_name }}</button>
     </div>
-    @for ($i = 1; $i < 256; $i++)
-        ping 172.16.1.{{ $i }}<br>
-    @endfor
     @if (!empty($put))
         <input type="hidden" name="_method" value="PUT">
     @endif
--- resources/views/horario/crear.blade.php
+++ resources/views/horario/crear.blade.php
@@ -24,6 +24,51 @@
                 @include('horario._form')
             </section>
         </div>
+        <div class="card">
+            <div class="card-body">
+                <table id="tbl_anio" class="table table-striped table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            @foreach ($dia_semanas as $value)
+                            <th>{{ $value->name }}</th>
+                            @endforeach
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            //print_r($anio);
+                        @endphp
+
+                        @foreach ($grupo_academicos as $value)
+                        <tr>@php
+                            $hr_inicio = strtotime ( env('HORA_INICIO_CLASES') ) ;
+                            $hr_inicio = strtotime ( env('HORA_ACADEMICA'), $hr_inicio ) ;
+                            $hr_inicio = date ( 'H:i' , $hr_inicio);
+                            for ($i=0; $i < env('NRO_HORA_ACADEMICA')+1; $i++){
+                                if ($hr_inicio!=env('HORA_REFRIGERIO')) {
+                                    echo '<option value="'.$hr_inicio.'"> '.$hr_inicio.' </option>';
+                                    $hr_inicio = strtotime ( env('HORA_ACADEMICA'), strtotime ($hr_inicio) ) ;
+                                    $hr_inicio = date('H:i',$hr_inicio);
+                                }else {
+                                    //echo '<option value="'.$hr_inicio.'"> '.$hr_inicio.' </option>';
+                                    $hr_inicio = strtotime ( env('TIEMPO_REFRIGERIO'), strtotime ($hr_inicio) ) ;
+                                    $hr_inicio = date('H:i',$hr_inicio);
+                                }
+
+                            }@endphp
+                            <td>{{ $value->id }}</td>
+                            <td>{{ $value->name }}</td>
+                            <td>{{ $value->grados->name }}</td>
+                            <td>{{ $value->seccions->name }}</td>
+                            <td>{{ $value->anio_academicos->name }}</td>
+                            <td>{{ $value->created_at }}</td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </main>
 
 @endsection
